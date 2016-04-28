41279bae7b9c508cdc77abd720fb226ada3015fd
unknown <mohamed.kamal20130214@gmail.com>
2016-04-28 21:25:40 +0200

    notification  and action service

--- Ramadan_Anaconda.sql
+++ Ramadan_Anaconda.sql
@@ -1,8 +1,8 @@
--- MySQL dump 10.13  Distrib 5.6.28, for debian-linux-gnu (x86_64)
+-- MySQL dump 10.13  Distrib 5.7.9, for Win64 (x86_64)
 --
--- Host: localhost    Database: se2firstapp
+-- Host: 127.0.0.1    Database: se2project
 -- ------------------------------------------------------
--- Server version	5.6.28-0ubuntu0.15.10.1
+-- Server version	5.7.10-log
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -16,13 +16,41 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `checkIn`
+-- Table structure for table `actions`
 --
 
-DROP TABLE IF EXISTS `checkIn`;
+DROP TABLE IF EXISTS `actions`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `checkIn` (
+CREATE TABLE `actions` (
+  `Aid` int(11) NOT NULL,
+  `actionid` int(11) NOT NULL,
+  `userid` int(11) NOT NULL,
+  `description` varchar(255) NOT NULL,
+  PRIMARY KEY (`Aid`),
+  KEY `userid` (`userid`),
+  CONSTRAINT `actions_ibfk_1` FOREIGN KEY (`userid`) REFERENCES `users` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `actions`
+--
+
+LOCK TABLES `actions` WRITE;
+/*!40000 ALTER TABLE `actions` DISABLE KEYS */;
+INSERT INTO `actions` VALUES (2,2,2,'like');
+/*!40000 ALTER TABLE `actions` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `checkin`
+--
+
+DROP TABLE IF EXISTS `checkin`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `checkin` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `user_id` int(11) DEFAULT NULL,
   `place_id` int(11) DEFAULT NULL,
@@ -33,41 +61,41 @@ CREATE TABLE `checkIn` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `checkIn`
+-- Dumping data for table `checkin`
 --
 
-LOCK TABLES `checkIn` WRITE;
-/*!40000 ALTER TABLE `checkIn` DISABLE KEYS */;
-INSERT INTO `checkIn` VALUES (1,5,1,'welcome new study year',0),(2,8,1,'back to school',0),(3,0,0,NULL,0),(4,18,3,'khalifa',0),(5,1,3,'wwww',0),(6,1,4,'wwww',0),(7,18,4,'wwww',0),(8,18,5,'wwww',0);
-/*!40000 ALTER TABLE `checkIn` ENABLE KEYS */;
+LOCK TABLES `checkin` WRITE;
+/*!40000 ALTER TABLE `checkin` DISABLE KEYS */;
+INSERT INTO `checkin` VALUES (1,5,1,'welcome new study year',0),(2,8,1,'back to school',0),(3,0,0,NULL,0),(4,18,3,'khalifa',0),(5,1,3,'wwww',0),(6,1,4,'wwww',0),(7,18,4,'wwww',0),(8,18,5,'wwww',0);
+/*!40000 ALTER TABLE `checkin` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
--- Table structure for table `comment_checkIn`
+-- Table structure for table `comment_checkin`
 --
 
-DROP TABLE IF EXISTS `comment_checkIn`;
+DROP TABLE IF EXISTS `comment_checkin`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `comment_checkIn` (
+CREATE TABLE `comment_checkin` (
   `user_id` int(11) NOT NULL,
   `checkIn_id` int(11) NOT NULL,
   `comment` varchar(1000) NOT NULL,
   KEY `user_id` (`user_id`),
   KEY `checkIn_id` (`checkIn_id`),
   CONSTRAINT `comment_checkIn_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`),
-  CONSTRAINT `comment_checkIn_ibfk_2` FOREIGN KEY (`checkIn_id`) REFERENCES `checkIn` (`id`)
+  CONSTRAINT `comment_checkIn_ibfk_2` FOREIGN KEY (`checkIn_id`) REFERENCES `checkin` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `comment_checkIn`
+-- Dumping data for table `comment_checkin`
 --
 
-LOCK TABLES `comment_checkIn` WRITE;
-/*!40000 ALTER TABLE `comment_checkIn` DISABLE KEYS */;
-INSERT INTO `comment_checkIn` VALUES (18,3,'Mohamed Ramadan Says Hay.'),(19,2,'first comment from post man'),(19,5,'second comment from post man');
-/*!40000 ALTER TABLE `comment_checkIn` ENABLE KEYS */;
+LOCK TABLES `comment_checkin` WRITE;
+/*!40000 ALTER TABLE `comment_checkin` DISABLE KEYS */;
+INSERT INTO `comment_checkin` VALUES (18,3,'Mohamed Ramadan Says Hay.'),(19,2,'first comment from post man'),(19,5,'second comment from post man');
+/*!40000 ALTER TABLE `comment_checkin` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -95,30 +123,60 @@ INSERT INTO `follow` VALUES ('m.samir','mhmdsamir92@gmail.com'),('mhmdsamir1@gma
 UNLOCK TABLES;
 
 --
--- Table structure for table `like_checkIn`
+-- Table structure for table `like_checkin`
 --
 
-DROP TABLE IF EXISTS `like_checkIn`;
+DROP TABLE IF EXISTS `like_checkin`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `like_checkIn` (
+CREATE TABLE `like_checkin` (
   `user_id` int(11) NOT NULL,
   `checkIn_id` int(11) NOT NULL,
   KEY `user_id` (`user_id`),
   KEY `checkIn_id` (`checkIn_id`),
   CONSTRAINT `like_checkIn_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`),
-  CONSTRAINT `like_checkIn_ibfk_2` FOREIGN KEY (`checkIn_id`) REFERENCES `checkIn` (`id`)
+  CONSTRAINT `like_checkIn_ibfk_2` FOREIGN KEY (`checkIn_id`) REFERENCES `checkin` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `like_checkIn`
+-- Dumping data for table `like_checkin`
 --
 
-LOCK TABLES `like_checkIn` WRITE;
-/*!40000 ALTER TABLE `like_checkIn` DISABLE KEYS */;
-INSERT INTO `like_checkIn` VALUES (19,5),(2,5);
-/*!40000 ALTER TABLE `like_checkIn` ENABLE KEYS */;
+LOCK TABLES `like_checkin` WRITE;
+/*!40000 ALTER TABLE `like_checkin` DISABLE KEYS */;
+INSERT INTO `like_checkin` VALUES (19,5),(2,5);
+/*!40000 ALTER TABLE `like_checkin` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `notification`
+--
+
+DROP TABLE IF EXISTS `notification`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `notification` (
+  `nid` int(11) NOT NULL AUTO_INCREMENT,
+  `userid` int(11) NOT NULL,
+  `checkinid` int(11) NOT NULL,
+  `description` varchar(255) NOT NULL,
+  PRIMARY KEY (`nid`),
+  KEY `userid` (`userid`),
+  KEY `checkinid` (`checkinid`),
+  CONSTRAINT `notification_ibfk_1` FOREIGN KEY (`userid`) REFERENCES `users` (`id`),
+  CONSTRAINT `notification_ibfk_2` FOREIGN KEY (`checkinid`) REFERENCES `checkin` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `notification`
+--
+
+LOCK TABLES `notification` WRITE;
+/*!40000 ALTER TABLE `notification` DISABLE KEYS */;
+INSERT INTO `notification` VALUES (1,1,2,'has comment'),(2,1,1,'make comment on post');
+/*!40000 ALTER TABLE `notification` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -135,7 +193,7 @@ CREATE TABLE `places` (
   `lat` double NOT NULL,
   `long` double NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -144,7 +202,7 @@ CREATE TABLE `places` (
 
 LOCK TABLES `places` WRITE;
 /*!40000 ALTER TABLE `places` DISABLE KEYS */;
-INSERT INTO `places` VALUES (1,'FCI','sdafaf',1223,1221),(2,'FCI1','Ramadan_Anaconda',1212,12126),(3,'FCI122','Ramadan_Anaconda',1212,12126),(4,'FCI1223','Ramadan_Anaconda3',1222,12226),(5,'FCI1223','Ramadan_Anaconda3',1222,12226),(6,'FCI1223','Ramadan_Anaconda3',1222,12226),(7,'cairo','has too many faculty ',0,123458),(8,'FCICU','Faculty of computer and informantion',0,1234);
+INSERT INTO `places` VALUES (1,'Cairo','is very beuitful',23,25);
 /*!40000 ALTER TABLE `places` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -158,10 +216,12 @@ DROP TABLE IF EXISTS `saveplaces`;
 CREATE TABLE `saveplaces` (
   `userid` int(11) NOT NULL,
   `placeid` int(11) NOT NULL,
-  KEY `placeid` (`placeid`),
+  `sid` int(11) NOT NULL,
+  PRIMARY KEY (`sid`),
   KEY `userid` (`userid`),
-  CONSTRAINT `saveplaces_ibfk_1` FOREIGN KEY (`placeid`) REFERENCES `places` (`id`),
-  CONSTRAINT `saveplaces_ibfk_2` FOREIGN KEY (`userid`) REFERENCES `users` (`id`)
+  KEY `placeid` (`placeid`),
+  CONSTRAINT `saveplaces_ibfk_1` FOREIGN KEY (`userid`) REFERENCES `users` (`id`),
+  CONSTRAINT `saveplaces_ibfk_2` FOREIGN KEY (`placeid`) REFERENCES `places` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -171,7 +231,7 @@ CREATE TABLE `saveplaces` (
 
 LOCK TABLES `saveplaces` WRITE;
 /*!40000 ALTER TABLE `saveplaces` DISABLE KEYS */;
-INSERT INTO `saveplaces` VALUES (1,1),(2,2),(3,2),(1,2),(1,2),(1,2),(1,2),(3,2),(2,2),(2,2),(2,2),(8,2),(8,2),(4,2),(4,2),(4,2),(4,2),(2,2),(1,2),(1,2),(1,2),(1,2),(1,2),(1,2),(1,2),(2,2),(2,2),(5,2),(5,2),(5,2),(5,2),(5,2),(5,2),(5,2),(5,2),(5,2),(5,2),(2,5),(5,2);
+INSERT INTO `saveplaces` VALUES (1,2,2),(1,2,3),(1,2,4),(1,2,5),(1,2,6),(1,2,7),(1,2,8),(1,2,9),(1,2,10),(1,2,11),(1,2,12),(1,2,13),(1,2,14),(1,2,15);
 /*!40000 ALTER TABLE `saveplaces` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -213,4 +273,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2016-04-20 22:25:57
+-- Dump completed on 2016-04-28 21:24:58
